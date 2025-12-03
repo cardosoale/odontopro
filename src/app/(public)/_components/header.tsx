4c313fc0b1d6fa11e011b910c0034f764efba80c
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2025-12-03 13:38:20 -0300

    feat: add header component with navigation and sheet functionality; update package.json and package-lock.json for new dependencies

---
+++ src/app/(public)/_components/header.tsx
@@ -0,0 +1,68 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { MenuIcon } from 'lucide-react';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navItems = [{ href: '#profissionais', label: 'Profissionais' }];
+
+  const NavLinks = () => (
+    <>
+      {navItems.map((item) => (
+        <Button
+          onClick={() => setIsOpen(false)}
+          key={item.href}
+          asChild
+          className='bg-transparent hover:bg-transparent
+                text-black
+                shadow-none'
+        >
+          <Link href={item.href}> {item.label}</Link>
+        </Button>
+      ))}
+    </>
+  );
+
+  return (
+    <header className='fixed top-0 right-0 left-0 z-999 py-4 px-6 bg-white'>
+      <div className='container mx-auto flex items-center justify-between'>
+        <Link href={'/'} className='text-3xl font-bold text-zinc-900'>
+          Odonto<span className='text-emerald-500'>Pro</span>
+        </Link>
+
+        <nav className='hidden md:flex items-center space-x-4'>
+          <NavLinks />
+        </nav>
+
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild className='md:hidden'>
+            <Button variant={'ghost'} size={'icon'}>
+              <MenuIcon className='w-6 h-6' />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side='right' className='w-240px sm:w-360px z-9999'>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+            <SheetDescription>
+              Veja nossos profissionais disponíveis:
+            </SheetDescription>
+            <nav className='flex flex-col space-y-4 mt-6'>
+              <NavLinks />
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
+  );
+}
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,9 @@
+import { Header } from './_components/header';
+
 export default function HomePage() {
   return (
     <main>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
+      <Header />
     </main>
   );
 }
